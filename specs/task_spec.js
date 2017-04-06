980f87678aecdafd783e4dd86d13363f6527b7f2
Suzanne <suzanne.aitchison@gmail.com>
2017-04-06 14:36:08 +0100

    has a difficulty level

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -5,6 +5,10 @@ describe('task tests', function(){
 
   var task = new Task("get the shopping in", 4)
 
+  it('has a description', function() {
+    assert.strictEqual('get the shopping in', task.description)
+  })
+
   it('has a difficulty level', function(){
     assert.strictEqual(4, task.difficulty)
   })

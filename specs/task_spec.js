9780150557fd05eaea9a1c0b93100dce050e714b
Suzanne <suzanne.aitchison@gmail.com>
2017-04-06 14:38:09 +0100

    has a reward value

--- specs/task_spec.js
+++ specs/task_spec.js
@@ -3,7 +3,7 @@ var Task = require('../task.js')
 
 describe('task tests', function(){
 
-  var task = new Task("get the shopping in", 4)
+  var task = new Task("get the shopping in", 4, 10)
 
   it('has a description', function() {
     assert.strictEqual('get the shopping in', task.description)
@@ -11,6 +11,10 @@ describe('task tests', function(){
 
   it('has a difficulty level', function(){
     assert.strictEqual(4, task.difficulty)
+  })
+
+  it('has a reward value', function(){
+    assert.strictEqual(10, task.reward)
   })
 
 
--- task.js
+++ task.js
@@ -1,6 +1,7 @@
-var Task = function(description, difficulty) {
+var Task = function(description, difficulty, reward) {
   this.description = description
   this.difficulty = difficulty
+  this.reward = reward
 }
 
 module.exports = Task
